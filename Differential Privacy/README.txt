7dda3db1282af4d4cc32d3f0edc5f40b9d491b32
joelyuhas <64444591+joelyuhas@users.noreply.github.com>
2021-02-19 18:57:11 -0500

    Differential Privacy
    Differential Privacy Addition

---
+++ Differential Privacy/README.txt
@@ -0,0 +1,18 @@
+Security In Machine Learning Differential Privacy Immplementation
+Joel Yuhas Portion
+
+
+The following code is based off of the tutorial created by Nicholas Papernot
+
+http://www.cleverhans.io/privacy/2019/03/26/machine-learning-with-differential-privacy-in-tensorflow.html
+
+
+
+This tutorial was modified to learn off cencus data in order to predict household income
+based on data characterisitics.
+
+
+The final implementation with results can be found in the "differential privacy implementation" 
+folder.
+
+Vedhas Patkar created the second portion of the project which is in his own repositroy.
---
+++ Differential Privacy/privacy-master/tutorials/data_execution.py
@@ -0,0 +1,56 @@
+import os
+#data path, learning rate, noise multiplier, batch size, microbatchsize, epochs
+
+##Epochs
+os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 10 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 10 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 50 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 500 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 1000 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 2000 1')
+##
+##Learngin Rate
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.30 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.50 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.75 1.1 128 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.60 1.1 128 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.90 1.1 128 1 100 1')
+#
+##
+###Batch Size
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 256 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 1024 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 512 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 2048 1 100 1')
+#
+##
+###Noise Multiplier
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.5 128 1 100 1')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 2.0 128 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 4.0 128 1 100 1')
+#os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 8.1 128 1 100 1')
+#
+#
+##
+##
+###Epochs No DP
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 1 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 10 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 50 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 500 0')
+##
+###Learngin Rate No DP
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.30 1.1 128 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.50 1.1 128 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.75 1.1 128 1 100 0')
+#
+###Batch Size
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 128 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 256 1 100 0')
+##os.system('python census_example.py "D:\Spooky\A-Documents\Code\Differential Privacy\data" 0.15 1.1 1024 1 100 0')
